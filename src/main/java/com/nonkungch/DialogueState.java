fa0c74c75cd302b6a7ba66267664cd7611eab96b
NonKun Ch <92234807+NonCraftStudio@users.noreply.github.com>
2025-10-05 11:27:46 +0700

    Create DialogueState.java

---
+++ src/main/java/com/nonkungch/DialogueState.java
@@ -0,0 +1,20 @@
+package com.nonkungch.dialogueseconomy;
+
+public class DialogueState {
+
+    private final String id;
+    private final String[] lines;
+    private int currentLine = 0;
+
+    public DialogueState(String id, String[] lines) {
+        this.id = id;
+        this.lines = lines;
+    }
+
+    public String getId() { return id; }
+    public String[] getLines() { return lines; }
+    public int getCurrentLineIndex() { return currentLine; }
+    public void incrementLine() { currentLine++; }
+    public boolean isFinished() { return currentLine >= lines.length; }
+    public String getCurrentLine() { return isFinished() ? null : lines[currentLine]; }
+  }
